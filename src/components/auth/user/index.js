bc1541a04b9f9e424f02e03849200fbb157ecbc3
BakhtiyarAvalov <Avalov90@mail.ru>
2023-08-17 18:56:37 +0600

    adjustment

--- src/components/auth/user/index.js
+++ src/components/auth/user/index.js
@@ -1,30 +1,37 @@
 'use client'
 
+import { useState } from "react"
+
+
 export default function UserLogin () {
+    const [step, setStep] = useState(1)
     return (
         <section className="login-page">
-            <div className="card">
+           
+           {step === 1 && <div className="card">
                 <h1>Поиск работы</h1>
                 <from>
                     <input className="input" placeholder="Введите e-mail"/>
-                    <button className="button button-primary">Продолжить</button>
+                    <button className="button button-primary" onClick={()=>setStep(2)}>Продолжить</button>
                 </from>
-            </div>
-            <div className="card">
+            </div>}
+
+            {step === 1 && <div className="card">
                 <h1>Поиск сотрудников</h1>
                 <p>Размещение вакансии и доступ к базе резюме</p>
                 <button className="button button-primary-border">Я ищу сотрудников</button>
-            </div>
-            <div className="card">
+            </div>}
+
+            {step === 2 && <div className="card">
                 <h1>Отправили код на...</h1>
                 <p>Напишите его, что бы подтвердить, что это вы, а не кто-то другой входит в личный кабинет </p>
                 <from>
                     <input className="input" placeholder="Введите код"/>
                     <p>Повторить можно через 00:48</p>
-                    <button className="button button-primary">Продолжить</button>
-                    <button className="button button-primary-border">Назад</button>
+                    <button className="button button-primary" >Продолжить</button>
+                    <button className="button button-primary-border" onClick={()=>setStep(1)}>Назад</button>
                 </from>
-            </div>
+            </div>}
         </section>
     )
 }
